2b3feaff26fe772b154f8609f58dc188caa5f8da
ShadeRenard <143022889+ShadeRenard@users.noreply.github.com>
2023-09-04 17:55:25 -0600

    sadie's example of work

---
+++ Basics/001 Conventions and Syntax/sadieswork.cs
@@ -0,0 +1,49 @@
+using System;
+					
+public class Program
+{
+	public Weapon miniGun;
+	public PowerUp weaponPowerUp;
+	
+	public void Main()
+	{
+		miniGun = new Weapon();
+		weaponPowerUp = new PowerUp();
+		
+		miniGun.ammoCount ++;
+		miniGun.firePower += weaponPowerUp.powerLevel;
+		Console.WriteLine("Sadie was Here!!");
+		Console.WriteLine("the fire power of " + miniGun.weaponName + " has an ammo count of " + miniGun.ammoCount + ".");
+		Console.WriteLine("it now has a fire power of " + miniGun.firePower + ", it has increased.");
+	}
+}
+public class Weapon
+{ 
+	public int ammoCount = 10;
+	public String weaponName = "mini Gun";
+	public float firePower = 11.5f;
+}
+public class PowerUp 
+{
+	public float powerLevel = 2.5f;
+}
+
+public class ClassExample
+{
+	
+}
+
+public class ClassWithVariables
+{
+public int x = 8 + 50;
+public string userName = "Sadie";
+}
+
+
+public class ClassWithFunctions
+{
+public void RunFunction()
+{
+Console.WriteLine("Running Function");
+}
+}
